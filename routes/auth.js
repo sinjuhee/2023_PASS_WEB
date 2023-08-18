f812f6f7dd0ba1374a2e7f6b9431b624c42079b9
diony0409@naver.com <diony0409@naver.com>
2023-08-18 16:32:21 +0900

    code for issue #3-login

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@
 module.exports = {
     CheckAuth: async function(req, res, next) {
         console.log('auth'); 
+        
         console.log('auth' );
         if(req.isAuthenticated()) {
             console.log("로그인 된 사용자입니다");
