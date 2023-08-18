06c49bf677cfc85fd7c3a47ad46ccc0e5b6fbdea
diony0409@naver.com <diony0409@naver.com>
2023-08-18 14:43:04 +0900

    modified by diony0409

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@
 
 module.exports = {
     CheckAuth: async function(req, res, next) {
+        console.log('auth');
         if(req.isAuthenticated()) {
             next();  //만약에 인증 확인 성공햇다면 다음 미들웨어로 넘겨주겠다 
         }
