b3d3e16aee2a47f61e9c17a472eca055b36150f0
diony0409@naver.com <diony0409@naver.com>
2023-08-18 15:36:46 +0900

    modified on a branch

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ module.exports = {
         console.log('auth'); 
         console.log('auth' );
         if(req.isAuthenticated()) {
-            console.log(req);
+            console.log("로그인 된 사용자입니다");
             next();  //만약에 인증 확인 성공햇다면 다음 미들웨어로 넘겨주겠다 
         }
         else {
